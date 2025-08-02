44da411e35c416988926befb9f4e5a4db5fa9fac
Kallyel <yagomigliorinidossantoskallyel@gmail.com>
2025-08-01 21:10:23 -0300

    PP=

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachaves');
 
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave)
+
+function mostraPalavrasChave() {
+  const texto = document.querySelector('#entrada-de-texto').value;
+  const campoResultado = document.querySelector('#resultado-palavrachave');
+  const PalavrasChave = processaTexto(texto);
+
+  campoResultado.textContent = palavras.join(", ");
+}
+
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contaFrequencia(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0, 10);
+}
+
+function contaFrequencia(palavras) {
+    let frequencias = [];
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (palavras[i] == palavras[j]) {
+                frequencias[i]++;
+            }
+        }
+    }
+    return frequencias;
+}
